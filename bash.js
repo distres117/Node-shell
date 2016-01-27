73f2a8b9b52e44b0c16f2ededc7772173bf8d80b
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-01-26 21:34:48 -0500

    Done through Command Arguments

---
+++ bash.js
@@ -0,0 +1,30 @@
+var command = require('./command.js');
+
+
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function(data) {
+	var input = data.toString().trim();
+	//Check to see if input contains an argument
+	var parts = input.split(' ');
+	input = parts[0];
+	var arg = parts[1];
+	var output = command[input]
+	if (output)
+		 output(callback, arg);
+	else
+		process.stdout.write("Invalid entry\nprompt > ");
+	//console.log(output());
+	// if (output())
+	// 	return output();
+	// else
+	// 	return "Invalid entry";
+	
+});
+
+function callback(output){
+	process.stdout.write(output);
+	process.stdout.write('\nprompt > ');
+}
---
+++ command.js
@@ -0,0 +1,97 @@
+var fs = require('fs');
+// var myCommand = function(data){
+// 	var input = data.toString().trim();
+// 	var output;
+// 	if (input==="pwd"){
+// 		output = process.cwd();
+// 	};
+// 	if (input==="date"){
+// 		output = new Date().toString();
+// 	};
+// 	process.stdout.write(output || "Invalid entry");	
+//   ;
+// }
+function readFile(cb, filename){ 
+	fs.readFile(filename, "utf8", function(err, file){
+		if (err)
+			throw new Error(err.message);
+		cb(file);
+	})
+}
+
+
+
+
+module.exports = {
+	pwd: function(cb){
+		cb(process.cwd());
+	},
+	date: function(cb){
+		cb(new Date().toString());
+	},
+	ls: function(callback){
+		var rtn =[];
+		fs.readdir('.', function(err, files) {
+		  if (err) throw err;
+		  files.forEach(function(file) {
+		    rtn.push(file);
+		  });
+			callback(rtn.join(" "));
+		});
+	},
+	echo: function(cb, arg){
+		cb(arg);
+	},
+	cat: function(cb, arg){
+		readFile(cb, arg);
+	},
+	head: function(cb, arg){
+		readFile(function(file){
+			var fileCut = file.split('\n').filter(function(item, index){
+				if (index < 5)
+					return item;
+			});
+			cb(fileCut.join('\n'));
+		}, arg);
+	},
+	tail: function(cb, arg){
+		readFile(function(file){
+			var fileCut = file.split('\n').filter(function(item, index, arr){
+				if (index > arr.length-6)
+					return item;
+			});
+			cb(fileCut.join('\n'));
+		}, arg);
+
+	},
+	sort: function(cb, arg){
+		readFile(function(file){
+			var fileCut = file.split('\n').sort();
+			cb(fileCut.join("\n"));
+		}, arg);
+	},
+	wc: function(cb, arg){
+		readFile(function(file){
+			var fileLength = file.split('\n').length;
+			cb(fileLength.toString());
+		}, arg);
+	},
+	uniq: function(cb, arg){
+		readFile(function(file){
+			var arr = [];
+			var lines = file.split('\n');
+			var lastLine;
+			for (var i in lines){
+				if (lines[i] != lastLine){
+					arr.push(lines[i]);
+					lastLine=lines[i];
+				}
+			}
+			cb(arr.join("\n"));
+		}, arg);
+	}
+}
+
+
+
+
